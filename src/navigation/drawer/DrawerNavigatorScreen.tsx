568e9c1f49337768cee2657412effd4e03684c07
lumesh2911-2 <lumesh@beeonline.co.in>
2026-07-08 08:23:15 +0530

    Stack and bottom navigation added.

---
+++ src/navigation/drawer/DrawerNavigatorScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DrawerNavigatorScreen = () => {
+  return (
+    <View>
+      <Text>DrawerNavigatorScreen</Text>
+    </View>
+  )
+}
+
+export default DrawerNavigatorScreen
